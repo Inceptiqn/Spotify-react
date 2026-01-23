dd87810d305fd1702bd0432e86cf194f7db15419
ACETI ALESSANDRO <79703273@castelli.local>
2026-01-23 13:51:09 +0100

    formatted files

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import DisplayHome from "./DisplayHome";
 
 const Display = () => {
   return (
-    <div className='w-full m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
-
+    <div className="w-full m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
+      <Routes>
+        <Route path="/" element={<DisplayHome />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default Display
+export default Display;
