a8c2b0e3f84adf11efcd09d5a8b05814e30ff26f
ACETI ALESSANDRO <79703273@castelli.local>
2026-02-13 13:49:55 +0100

    started audio impl, check app

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import Sidebar from "./Sidebar";
 import { UI, SONGS, ALBUMS } from "../assets/assets";
+import { PlayerContext } from "../context/PlayerContext";
 
 const Player = () => {
+  const { seekBar, seekBg, playStatus, play, pause } =
+    useContext(PlayerContext);
+
   const getSongAlbumCover = (songIndex) => {
     const song = SONGS[songIndex];
     if (!song) return null;
@@ -58,6 +62,14 @@ const Player = () => {
             >
               <path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288z" />
             </svg>
+            <svg
+              role="img"
+              aria-hidden="true"
+              viewBox="0 0 16 16"
+              className="w-4 h-4 fill-current"
+            >
+              <path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7z" />
+            </svg>
           </button>
           <button className="w-8 h-8 flex items-center justify-center cursor-pointer text-gray-500 hover:text-white">
             <svg
@@ -82,8 +94,14 @@ const Player = () => {
         </div>
         <div className="flex  items-center gap-3">
           <p className="text-xs text-gray-400">1:06</p>
-          <div className="w-[70vw] max-w-150 bg-gray-500 rounded-full cursor-pointer">
-            <hr className="h-1 border-none w-72 bg-white hover:bg-[#1db954] rounded-full" />
+          <div
+            ref={seekBg}
+            className="w-[70vw] max-w-150 bg-gray-500 rounded-full cursor-pointer"
+          >
+            <hr
+              ref={seekBar}
+              className="h-1 border-none w-72 bg-white hover:bg-[#1db954] rounded-full"
+            />
           </div>
           <p className="text-xs text-gray-400">3:20</p>
         </div>
